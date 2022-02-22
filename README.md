68f27705bc3a10601e11e094834a9ac6dcf9a0e5
Daly Olivert <92411950+dalyo1212@users.noreply.github.com>
2022-02-22 03:02:18 +0000

    Did the lab

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+The programs functionality is to have two turtles race, once thats done one of the turtles draws creates shapes
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ I was not able to complete part 1 and 2 of A. I got confused and decided to move on and continue with part B.
 
 ## REFERENCES:
- < List any outside resources used >
+ N/A
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ N/A
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 #Part A
 window = turtle.Screen()       # 2.  Create a screen
-window.bgcolor('lightblue')
+window.bgcolor('pink')
 
 michelangelo = turtle.Turtle()    # 3.  Create two turtles
 leonardo = turtle.Turtle()
@@ -17,10 +17,32 @@ leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
+
 ## 5. your code goes here
-
-
+calls = (10)
+for i in range(calls):
+  x = random.randrange(1,10)
+  for s in range(x):
+    michelangelo.forward(x)
+    leonardo.forward(x)
 # Part B - complete part B here
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+michelangelo.down()
+shapes = [3,4,6,9,12]
+list_size = len(shapes)
+side_length = 20
+go_forward = 40
+
+for i in range (list_size):
+    for s in range(shapes[i]):
+      angle = 360/shapes[i]
+      michelangelo.down()
+      michelangelo.right(angle)
+      michelangelo.forward(side_length)
+    michelangelo.up()
+    michelangelo.forward(go_forward)
 
 
 window.exitonclick()
